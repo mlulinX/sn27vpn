cd02a88c94ba36ead034256dd1dea7869b361f95
SN <50373900+mlulinX@users.noreply.github.com>
2023-09-23 11:03:05 +0630

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+plain='\033[0m'
+
+#စာလုံးအရောင်များ(Server Message)
+red='\e[31m'    #အနီရောင်
+yellow='\e[33m' #အဝါရောင်
+gray='\e[90m'   #မီးခိုးရောင်
+green='\e[92m'  #အစိမ်းရောင်
+blue='\e[94m'   #အပြာရောင်
+magenta='\e[95m'#ပန်းခရမ်းရောင်
+cyan='\e[96m'   #စိမ်းပြာရောင်
+none='\e[0m'    #အရောင်မရှိ
+
+username="snxsn"
+password="snxsn"
+sshlimiter="600"
+dias="2"
+servermessage="<h3><font color='red'>
+Created by Skyn®SN (@mlulinX)
+</font></h3>"
+
+
+[[ $EUID -ne 0 ]] && echo -e "${red}Error: ${plain} You must use root user to run this script!\n" && exit 1
+
+sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_skyn/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
+echo "$servermessage" | tee /etc/ssh/gcp_skyn >/dev/null
+final=$(date "+%Y-%m-%d" -d "+$dias days")
+gui=$(date "+%d/%m/%Y" -d "+$dias days")
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -e $final -M -s /bin/false -p $pass $username >/dev/null
+echo "$password" >/etc/$username
+echo "$username:$password" | chpasswd
+echo "$username $sshlimiter" >>/root/usuarios.db
+IP=$(wget -qO- ipv4.icanhazip.com)
+echo ""
+echo -e "\033[1;32m===================================="
+echo -e "\033[1;32m   🇲🇲 SN FREENETㅤ 🇲🇲  " 
+echo -e "\033[1;32m===================================="
+echo ""
+echo -e "\033[1;37m◈─────⪧ SSH ACCOUNT ⪦─────◈"
+echo ""
+echo -e "\033[1;32m◈ Host / IP   :⪧  \033[1;31m$IP"
+echo -e "\033[1;32m◈ Port        :⪧  \033[1;31m22"
+echo -e "\033[1;32m◈ Username    :⪧  \033[1;31m$username"
+echo -e "\033[1;32m◈ Password    :⪧  \033[1;31m$password"
+echo -e "\033[1;32m◈ Login Limit :⪧  \033[1;31m$sshlimiter"
+echo -e "\033[1;32m◈ Expire Date :⪧  \033[1;31m$gui"
+echo ""
+echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
+
+echo -e "${cyan} Success - ${plain}"
