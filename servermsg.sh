fc09273ade61443aad4c3b94e683de6cecfa9365
SN <50373900+mlulinX@users.noreply.github.com>
2024-06-28 15:38:00 +0630

    Update servermsg.sh

--- servermsg.sh
+++ servermsg.sh
@@ -1,3 +1,17 @@
+red='\e[31m'    #အနီရောင်
+yellow='\e[33m' #အဝါရောင်
+gray='\e[90m'   #မီးခိုးရောင်
+green='\e[92m'  #အစိမ်းရောင်
+blue='\e[94m'   #အပြာရောင်
+magenta='\e[95m'#ပန်းခရမ်းရောင်
+cyan='\e[96m'   #စိမ်းပြာရောင်
+none='\e[0m'    #အရောင်မရှိ
+
+
+apt-get update
+
+
+
 servermessage="<h3><font color='red'>
 Created by https://t.me/mlulinX
 </font></h3>"
