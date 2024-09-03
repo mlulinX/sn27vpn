7565d71800f61c9332586493b1ff09ac553dd28f
SN <50373900+mlulinX@users.noreply.github.com>
2024-09-03 13:07:28 +0630

    Update servermsg.sh

--- servermsg.sh
+++ servermsg.sh
@@ -13,7 +13,8 @@ apt-get update
 
 
 servermessage="<h3><font color='red'>
-Created by https://t.me/mlulinX
+🇲🇲 🇲🇲 Powered by SN
+Telegram: https://t.me/mlulinX
 </font></h3>"
 sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/snpro/' /etc/ssh/sshd_config
 echo "$servermessage" | tee /etc/ssh/snpro >/dev/null
